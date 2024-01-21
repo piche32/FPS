12fcd8002429ee68d4a3d5a4a3083544c40f0d58
piche32 <piche32@naver.com>
2024-01-22 00:27:25 +0900

    Dog 클래스에 TakeDamage 함수 추가

--- Source/FPS/Dog.cpp
+++ Source/FPS/Dog.cpp
@@ -1,34 +1,42 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Dog.h"
 
 // Sets default values
 ADog::ADog()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ADog::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Health = MaxHealth;
 }
 
 // Called every frame
 void ADog::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
-void ADog::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ADog::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+float ADog::TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController *EventInstigator, AActor *DamageCauser)
+{
+	float DamageToApply = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	DamageToApply = FMath::Min(Health, DamageToApply);
+	Health -= DamageToApply;
+
+	UE_LOG(LogTemp, Warning, TEXT("Dog Health is %f"), Health);
+
+	return DamageToApply;
+}
--- Source/FPS/Dog.h
+++ Source/FPS/Dog.h
@@ -19,11 +19,19 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController *EventInstigator, AActor *DamageCauser) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth = 100;
+
+	UPROPERTY(VIsibleAnywhere)
+	float Health;
 };
