82e2526008bbeec95b41988e3845c2139a908c43
sohenn <piche32@mail.hongik.ac.kr>
2024-02-18 16:38:58 +0900

    Dog에 죽는 애니메이션 추가

--- Source/FPS/HealthComponent.h
+++ Source/FPS/HealthComponent.h
@@ -26,8 +26,11 @@ public:
 private:
 	UPROPERTY(EditDefaultsOnly)
 	float MaxHealth = 100.f;
-	float Health = 0.f;
+	float Health = 1.f;
 
 	UFUNCTION()
 	void DamageTaken(AActor *DamagedActor, float Damage, const UDamageType *DamageType, class AController *Instigator, AActor *DamageCauser);
+
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
 };
