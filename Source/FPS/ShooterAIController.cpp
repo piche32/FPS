fe41cd9e4f02aa08acd9ab4f06f7603a0d598cbc
piche32 <piche32@naver.com>
2024-03-17 20:03:27 +0900

    Shooter AI 생성
    - ShooterAIController 생성
    - Behavior Tree Shooter 생성
    - Blackboard Shooter 생성

---
+++ Source/FPS/ShooterAIController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    SetFocus(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
+}
