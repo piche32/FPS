01e6f01dbc15fef5855ce55481ea2713344db478
piche32 <piche32@naver.com>
2024-03-21 02:31:58 +0900

    BP_InteractiveDoor 블루프린트로 구현한 것을 C++로 수정

--- Source/FPS/InteractiveDoor.cpp
+++ Source/FPS/InteractiveDoor.cpp
@@ -1,27 +1,117 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "InteractiveDoor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AInteractiveDoor::AInteractiveDoor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrame"));
+	SetRootComponent(DoorFrame);
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(DoorFrame, TEXT("DoorPoint"));
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	Collision->SetupAttachment(DoorFrame);
 }
 
 // Called when the game starts or when spawned
 void AInteractiveDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (Collision)
+	{
+		Collision->OnComponentBeginOverlap.AddDynamic(this, &AInteractiveDoor::OnOpen);
+		Collision->OnComponentEndOverlap.AddDynamic(this, &AInteractiveDoor::OnClose);
+	}
 }
 
 // Called every frame
 void AInteractiveDoor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void AInteractiveDoor::OnOpen(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OhterComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	APawn *OtherPawn = Cast<APawn>(OtherActor);
+	if (!OtherPawn || !OtherPawn->IsPlayerControlled())
+	{
+		return;
+	}
+
+	FVector Direction = Door->GetComponentLocation() - OtherPawn->GetActorLocation();
+	RotatingForward = UKismetMathLibrary::Vector_HeadingAngle(Direction) < 0;
+	TargetAngle = RotatingForward ? abs(TargetAngle) : -abs(TargetAngle);
+
+	if (CloseTimer.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(CloseTimer);
+	}
+	GetWorldTimerManager().SetTimer(
+		OpenTimer,
+		this,
+		&AInteractiveDoor::Opening,
+		GetWorld()->GetDeltaSeconds(),
+		true);
+}
+
+void AInteractiveDoor::OnClose(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex)
+{
+	APawn *OtherPawn = Cast<APawn>(OtherActor);
+	if (!OtherPawn || !OtherPawn->IsPlayerControlled())
+	{
+		return;
+	}
+
+	if (OpenTimer.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(OpenTimer);
+	}
+
+	GetWorldTimerManager().SetTimer(
+		CloseTimer,
+		this,
+		&AInteractiveDoor::Closing,
+		GetWorld()->GetDeltaSeconds(),
+		true);
+}
+
+void AInteractiveDoor::Opening()
+{
+	FRotator Target(0.f, TargetAngle, 0.f);
+	Door->SetRelativeRotation(
+		FMath::RInterpTo(
+			Door->GetRelativeRotation(),
+			Target,
+			GetWorld()->GetDeltaSeconds(),
+			RotatingSpeed));
+
+	if (abs(Target.Yaw - Door->GetRelativeRotation().Yaw) < 1)
+	{
+		GetWorldTimerManager().ClearTimer(OpenTimer);
+	}
+}
+
+void AInteractiveDoor::Closing()
+{
+	Door->SetRelativeRotation(
+		FMath::RInterpTo(
+			Door->GetRelativeRotation(),
+			FRotator(0.f, 0.f, 0.f),
+			GetWorld()->GetDeltaSeconds(),
+			RotatingSpeed));
+
+	if (abs(Door->GetRelativeRotation().Yaw) < 1)
+	{
+		GetWorldTimerManager().ClearTimer(CloseTimer);
+	}
+}
--- Source/FPS/InteractiveDoor.h
+++ Source/FPS/InteractiveDoor.h
@@ -6,12 +6,14 @@
 #include "GameFramework/Actor.h"
 #include "InteractiveDoor.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class FPS_API AInteractiveDoor : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AInteractiveDoor();
 
@@ -19,8 +21,37 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UBoxComponent *Collision;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *DoorFrame;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Door;
+
+	FTimerHandle OpenTimer;
+	FTimerHandle CloseTimer;
+
+	bool RotatingForward;
+
+	UPROPERTY(EditAnywhere, Category = "Rotating")
+	float TargetAngle = 90.f;
+
+	UPROPERTY(EditAnywhere, Category = "Rotating")
+	float RotatingSpeed = 1.f;
+
+	UFUNCTION()
+	void OnOpen(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OhterComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnClose(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex);
+
+	void Opening();
+	void Closing();
 };
