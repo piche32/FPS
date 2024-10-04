5bfad70f99a274ae74bbfb6547302a9f86994359
piche32 <piche32@naver.com>
2024-10-04 23:12:39 +0900

    BP_ExploreGameMode 생성

---
+++ Source/FPS/ExploreGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExploreGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API AExploreGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
